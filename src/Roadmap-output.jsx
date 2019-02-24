061b13f350c5ab1ad9ccab6b7e6a61439e035f70
Eleanor Mollett <eleanor.mollett@gmail.com>
2019-02-24 18:47:02 +0000

    added text for last label, and made all sizes dependent on the canvas size, so that later I can resize this and everythign will follow

--- src/Roadmap-output.jsx
+++ src/Roadmap-output.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const canvasSize = 400; //this will be variable later - hence changing other variables to be dependent on this
+
 class Output extends Component {
 
   componentDidMount() {
@@ -21,55 +23,59 @@ class Output extends Component {
     var wordYPos; //the Y position of the words
     var nextText; //changes second text
     var laterText; //changes third text
+    var lastText; //changes fourth text
     var xPos; //switch between horizontal and vertical
     var yPos; //switch between horizontal and vertical
     var align; //word alignment
 
     if (this.props.selectedRadiusOption == "option1"){
-        radiusA = 100;
-        radiusB = 300;
+        radiusA = 0.25*canvasSize;
+        radiusB = 0.75*canvasSize;
         radiusC = 0;
         nextText = "Next";
         laterText = "Later";
+        lastText = "";
       }else if (this.props.selectedRadiusOption == "option2"){
-        radiusA = 100;
-        radiusB = 250;
-        radiusC = 400;
+        radiusA = 0.25*canvasSize;
+        radiusB = 0.5*canvasSize;
+        radiusC = 0.75*canvasSize;
         nextText = "Next";
         laterText = "Later";
+        lastText = "Verify";
       }else if (this.props.selectedRadiusOption == "option3") {
-        radiusA = 100;
-        radiusB = 225;
-        radiusC = 350;
+        radiusA = 0.25*canvasSize;
+        radiusB = 0.5*canvasSize;
+        radiusC = 0.75*canvasSize;
         nextText = "Next month";
-        laterText = "3 months"
+        laterText = "3 months";
+        lastText = "6 months";
       };
 
     if (this.props.selectedArcOption == "option4"){
         centerPos = 0;
         radiusFactor= 1;
-        wordXPos = 4;
-        wordYPos = 16;
+        wordXPos = canvasSize/100;
+        wordYPos = canvasSize/25;
         xPos = 1;
         yPos = 0;
         align = "start";
       }else if (this.props.selectedArcOption == "option5"){
-        centerPos = 200;
+        centerPos = canvasSize/2;
         radiusFactor = 0.5;
         wordXPos = 0;
-        wordYPos = 16;
+        wordYPos = canvasSize/25;
         xPos = 0;
         yPos = 1;
         align = "center";
       }
 
-    this.updateCanvas(radiusA, radiusB, radiusC, centerPos, radiusFactor, wordXPos, wordYPos, nextText, laterText, xPos, yPos, align);
+    this.updateCanvas(radiusA, radiusB, radiusC, centerPos, radiusFactor, wordXPos, wordYPos, nextText, laterText, lastText, xPos, yPos, align);
   }
 
-  updateCanvas(radiusA, radiusB, radiusC, centerPos, radiusFactor, wordXPos, wordYPos, nextText, laterText, xPos, yPos, align) {
+  updateCanvas(radiusA, radiusB, radiusC, centerPos, radiusFactor, wordXPos, wordYPos, nextText, laterText, lastText, xPos, yPos, align) {
     console.log("centerPos is " + centerPos);
       const ctx = this.refs.canvas.getContext("2d");
-        ctx.clearRect(0 ,0, 400, 400);
+        ctx.clearRect(0 ,0, canvasSize, canvasSize);
 
       const ctxA = this.refs.canvas.getContext('2d');
         ctxA.beginPath();
@@ -92,21 +98,26 @@ class Output extends Component {
         ctxNow.fillText("Now", centerPos+(wordXPos), centerPos+(wordYPos)+(-0.5*wordYPos*yPos));
 
       const ctxNext = this.refs.canvas.getContext('2d');
-        ctxNow.font = (16) + "px Arial";
+        ctxNext.font = (16) + "px Arial";
         ctx.textAlign = align;
-        ctxNow.fillText(nextText, centerPos+(radiusA*radiusFactor*xPos)+(wordXPos), centerPos+(radiusA*radiusFactor*yPos)+(wordYPos));
+        ctxNext.fillText(nextText, centerPos+(radiusA*radiusFactor*xPos)+(wordXPos), centerPos+(radiusA*radiusFactor*yPos)+(wordYPos));
 
       const ctxLater = this.refs.canvas.getContext('2d');
-        ctxNow.font = (16) + "px Arial";
+        ctxLater.font = (16) + "px Arial";
         ctx.textAlign = align;
-        ctxNow.fillText(laterText, centerPos+(radiusB*radiusFactor*xPos)+(wordXPos), centerPos+(radiusB*radiusFactor*yPos)+(wordYPos));
+        ctxLater.fillText(laterText, centerPos+(radiusB*radiusFactor*xPos)+(wordXPos), centerPos+(radiusB*radiusFactor*yPos)+(wordYPos));
+
+      const ctxLast = this.refs.canvas.getContext('2d');
+        ctxLast.font = (16) + "px Arial";
+        ctx.textAlign = align;
+        ctxLast.fillText(lastText, centerPos+(radiusC*radiusFactor*xPos)+(wordXPos), centerPos+(radiusC*radiusFactor*yPos)+(wordYPos));
   }
 
   render() {
     return (
       <div className="outputContainer">
         <p>Output</p>
-          <canvas ref="canvas" width={400} height={400} className="canvas" />
+          <canvas ref="canvas" width={canvasSize} height={canvasSize} className="canvas" />
       </div>
     );
   }
